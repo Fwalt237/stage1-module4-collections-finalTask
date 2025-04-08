b3275db09b650fc2f48129dfd735c4cac0f807ba
Fwalt237 <patrickkamwa17@gmail.com>
2025-04-09 00:59:03 +0100

    finalTask

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class DeveloperProjectFinder {
+public class DeveloperProjectFinder implements Comparator<String> {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> developers = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                developers.add(entry.getKey());
+            }
+        }
+        developers.sort(this);
+        return developers;
+    }
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length()==o2.length()){
+            return o2.compareTo(o1);
+        }
+        return o2.length()-o1.length();
     }
 }
+
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,26 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<String, Integer> firstMap = new HashMap<>();
+        for (String key : sourceMap.keySet()) {
+            firstMap.put(key, key.length());
+        }
+        Map<Integer, Set<String>> finalMap = new HashMap<>();
+        for (Integer i : firstMap.values()) {
+            Set<String> set = new HashSet<>();
+            for (Map.Entry<String, Integer> entry : firstMap.entrySet()) {
+                if (i.equals(entry.getValue())) {
+                    set.add(entry.getKey());
+                }
+            }
+            finalMap.put(i, set);
+        }
+        return finalMap;
     }
 }
